3b8e6cf1dd0fe3b98e62d70ed2b65c888444a77d
rifkisetiawan0101 <rifki.setiawan0101@gmail.com>
2025-07-11 16:36:33 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from 'next';
-import type { Configuration } from 'webpack';
 
 const nextConfig: NextConfig = {
     images: {
@@ -12,7 +11,7 @@ const nextConfig: NextConfig = {
             },
         ],
     },
-
+    
     eslint: {
         ignoreDuringBuilds: true,
     },
@@ -21,17 +20,32 @@ const nextConfig: NextConfig = {
         ignoreBuildErrors: true,
     },
 
-    webpack(config: Configuration) {
-        if (config.module && config.module.rules) {
-            config.module.rules.push({
+    webpack(config) {
+        // Menambahkan pemeriksaan keamanan untuk 'rules'
+        if (!config.module.rules) {
+            return config;
+        }
+
+        // Cari aturan default untuk SVG
+        // @ts-ignore - Kita mengabaikan tipe 'rule' yang rumit
+        const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'));
+
+        config.module.rules.push(
+            {
                 test: /\.svg$/i,
                 issuer: /\.[jt]sx?$/,
                 use: ['@svgr/webpack'],
-            });
+            }
+        );
+        
+        // Ubah aturan default jika ditemukan
+        if (fileLoaderRule) {
+            // @ts-ignore - Kita memodifikasi properti yang ada
+            fileLoaderRule.exclude = /\.svg$/i;
         }
 
         return config;
     },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
