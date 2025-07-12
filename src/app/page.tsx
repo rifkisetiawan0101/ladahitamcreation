1645c5c6648c3f8e1ddd3cf09dbd462b75cd3788
rifkisetiawan0101 <rifki.setiawan0101@gmail.com>
2025-07-13 01:21:58 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -53,7 +53,7 @@ export default async function HomePage() {
             <h2 className="font-display text-4xl text-center text-amber-300 mb-12">
                 Featured Projects
             </h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8">
                 {projects.map((project) => (
                     <ProjectCard key={project.id} project={project} />
                 ))}
@@ -77,7 +77,7 @@ export default async function HomePage() {
             <h2 className="font-display text-4xl text-center text-amber-300 mb-12">
             Hall of Fame
             </h2>
-            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-10">
+            <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-x-12 gap-y-10">
                 {achievements.map((achievement) => (
                     <AchievementItem key={achievement.id} achievement={achievement} />
                 ))}
